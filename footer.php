a939cc8ae3c4babc25132061f257bef85731c080
vijaynkhot <khotvijayn@gmail.com>
2016-11-04 14:40:40 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="alert alert-info">
+    <h2>Footer goes here</h2>
+	</div>
+</div>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Created by Vijay : Using PDO, OOPS, Bootstrap</title>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen"> 
+</head>
+
+<body>
+
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+ 
+        <div class="navbar-header">
+           
+        </div>
+ 
+    </div>
+</div>
